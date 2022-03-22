14e9b17aa808d4e2c32b7c9d713feeee82ee5bd0
Mariano JavaSwing <marianogoncalves8@gmail.com>
2022-03-22 20:57:00 +0100

    initial commit

---
+++ src/main/java/com/dev/wasp/pa4sft/WaspPa4SftApplication.java
@@ -0,0 +1,13 @@
+package com.dev.wasp.pa4sft;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WaspPa4SftApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WaspPa4SftApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/dev/wasp/pa4sft/WaspPa4SftApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dev.wasp.pa4sft;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WaspPa4SftApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
